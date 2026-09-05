11a1a860b675567eb3fa7fbe3225ef3644f96f88
V0X-PH4NT45M4 <ashutoshsamantaray430@gmail.com>
2026-09-06 00:27:29 +0530

    add resource and dev-cards signature to hpp

---
+++ include/engine.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace engine {
+	enum class Resource {
+		ORE,
+		WHEAT,
+		BRICK,
+		WOOD,
+		SHEEP,
+	};
+
+	enum class DevCard {
+		KNIGHT,
+		ROAD_BUILDER,
+		MONOPOLY,
+		VICTORY_POINT,
+		YEAR_OF_PLENTY,
+	}
+}
